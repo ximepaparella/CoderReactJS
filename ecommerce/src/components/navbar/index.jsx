3c0e90944382a229ebb5057697479cdd4e1b4b6e
XPaparella <XPaparella@LN42210089>
2021-01-25 13:13:03 -0300

    Se suma el componente Navbar

---
+++ ecommerce/src/components/navbar/index.jsx
@@ -0,0 +1,16 @@
+import './navbar.scss'
+
+const NavBar = () => {
+  return (
+    <>
+        <nav className="com-navbar">
+            <a>Link</a>
+            <a>Link</a>
+            <a>Link</a>
+            <a>Link</a>
+        </nav>
+    </>
+  );
+}
+
+export default NavBar;
